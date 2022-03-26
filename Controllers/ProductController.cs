39630a49e91b7904789a3e1919d4a44d199b2f90
utshaav <utshaav583@gmail.com>
2022-03-26 12:16:45 +0545

    Product CRUD completed

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,7 +1,78 @@
 using Microsoft.AspNetCore.Mvc;
+using SalesTransaction.Data;
+using SalesTransaction.Models;
+using SalesTransaction.Services;
 
 namespace SalesTransaction.Controllers;
 public class ProductController : Controller
 {
+    private ProductServices productServices; 
+    public ProductController(TestDBContext context)
+    {
+        productServices= new ProductServices(context);
+    }
+    public IActionResult Index()
+    {
+        List<Product> products = productServices.SelectAll(); 
+        return View(products);
+    }
+
+    public IActionResult Detail(int id)
+    {
+        Product product = productServices.SelectById(id);
+        return View(product);
+    }
+    public IActionResult Add()
+    {
+        return PartialView(new Product());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAsync(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            await productServices.CreateAsync(product);
+            return RedirectToActionPermanent("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Update(int id)
+    {
+        Product product = productServices.SelectById(id);
+        return PartialView(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            await productServices.UpdateAsync(product);
+            // Console.WriteLine(result);
+            return RedirectPermanent("Index");
+        }
+        return View(product);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        Product product = productServices.SelectById(id);
+        return PartialView(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(Product product)
+    {
+        await productServices.DeleteById(product.Id);
+        return RedirectToActionPermanent("Index");
+    }
+
+    public async Task<IActionResult> ConfirmDelete(int id)
+    {
+        await productServices.DeleteById(id);
+        return RedirectToActionPermanent("Index");
+    }
 
 }
--- Services/ProductServices.cs
+++ Services/ProductServices.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using SalesTransaction.Data;
+using SalesTransaction.Models;
 
 namespace SalesTransaction.Services;
 public class ProductServices
@@ -7,6 +9,80 @@ public class ProductServices
     public ProductServices(TestDBContext context)
     {
         this.context = context;
+    }
 
+    public List<Product> GetAll()
+    {
+        context.Database.ExecuteSqlRawAsync("");
+        return new List<Product>();
+    }
+
+    public async Task CreateAsync(Product product)
+    {
+        try
+        {
+            string query = StoredProcedureServices.GenerateQueryString(product, "usp_Product_Insert", 1);
+            await context.Database.ExecuteSqlRawAsync(query);
+            await context.SaveChangesAsync();
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+    }
+
+    public async Task UpdateAsync(Product product)
+    {
+        try
+        {
+            string query = StoredProcedureServices.GenerateQueryString(product, "usp_Product_Insert", 0);
+            await context.Database.ExecuteSqlRawAsync(query);
+            await context.SaveChangesAsync();
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+    }
+
+    public List<Product> SelectAll()
+    {
+        List<Product> customers = new List<Product>();
+        try
+        {
+            customers = context.Products.FromSqlRaw($"usp_Product_Select @flag = 1").ToList();
+            return customers;
+        }
+        catch (System.Exception)
+        {
+
+            return customers;
+        }
+    }
+
+    public Product SelectById(int id)
+    {
+        try
+        {
+            return context.Products.FromSqlRaw($"usp_Product_Select @flag = 1, @Id = {id}").ToList().FirstOrDefault()!;
+        }
+        catch (System.Exception)
+        {
+            return new Product();
+        }
+    }
+
+    public async Task<string> DeleteById(int id)
+    {
+        try
+        {
+            await context.Database.ExecuteSqlRawAsync($"usp_Product_Select @flag = 0, @Id = {id}");
+            context.SaveChanges();
+            return "Success";
+        }
+        catch (System.Exception)
+        {
+            return "Failed";
+        }
     }
 }
